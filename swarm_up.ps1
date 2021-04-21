54336cd23b2ee699cc2c8b59662bcdef794af685
YakHacks <jake.d.mcewan@outlook.com>
2021-04-20 23:50:29 -0400

    Delete swarm_up.ps1

--- swarm_up.ps1
+++
@@ -1,4 +0,0 @@
-# Initialize Swarm
-$join_command = (vagrant ssh docker-1 --command "docker swarm init || docker swarm join-token worker" | Select-String -Pattern "docker swarm join --token .*").Matches[0].Value
-vagrant ssh docker-2 --command "$join_command"
-vagrant ssh docker-3 --command "$join_command"
